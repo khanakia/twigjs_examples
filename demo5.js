21d03dfbcee71e47d495073ba8685642d5116bbd
khanakia <khanakia@gmail.com>
2020-07-16 14:43:07 +0530

    more examples

--- demo5.js
+++ demo5.js
@@ -2,8 +2,12 @@ const Twig = require('twig')
 const {twig } = Twig
 
 var template = twig({
-  base: 'views/demo1',
-  path: 'index.twig',
+  path: 'views/demo5/page.twig',
+  async: false,
+  allowInlineIncludes: true,
+  namespaces: { 
+    'base': '/Volumes/D/www/js/twigjs_examples/views/demo5/',
+  },
   load(template) {
     const output = template.render();
     console.log(output)
